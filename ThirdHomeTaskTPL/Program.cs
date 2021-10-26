39a34ad37cad321f85d13fe8936d804f885ac75b
Iliya Taudd <iliya2002pl.ru@gmail.com>
2021-10-26 16:35:19 +0300

    Add project files.

---
+++ ThirdHomeTaskTPL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThirdHomeTaskTPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
